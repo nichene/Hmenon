8d66cc6a88887e2339f63a8ca45671f8d9ad0ed1
Reddstar <ricardolucas1196@gmail.com>
2015-06-23 12:38:17 -0300

    Pesquisar funcionando :D

--- Hmenon2/app/src/main/java/com/ufrpe/hmenon/infrastructure/gui/MainActivity.java
+++ Hmenon2/app/src/main/java/com/ufrpe/hmenon/infrastructure/gui/MainActivity.java
@@ -7,7 +7,9 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
 import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,7 +21,6 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,6 +39,7 @@ import com.ufrpe.hmenon.user.gui.MainLogin;
 import com.ufrpe.hmenon.user.service.UserBusiness;
 import java.lang.reflect.Field;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
@@ -45,6 +47,7 @@ public class MainActivity extends ActionBarActivity {
     private UserBusiness userService;
     private TouristicPointBusiness touristicPointBusiness;
     private List<TouristicPoint> touristicPoints;
+    private List<TouristicPoint> allPoints;
     private ListView lista;
     private GPSTracker gps;
     private Context currentContext = MainActivity.this;
@@ -67,16 +70,15 @@ public class MainActivity extends ActionBarActivity {
         lista = (ListView) findViewById(R.id.listPoints);
         touristicPointBusiness = new TouristicPointBusiness(MainInitial.getContext());
         touristicPoints = touristicPointBusiness.checkGetAll();
+        allPoints = touristicPoints;
         populate();
         getOverflowMenu();
         userService = new UserBusiness(MainInitial.getContext());
-        search = (SearchView) findViewById(R.id.search);
 
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 TouristicPoint touristicPoint = touristicPoints.get(position);
-
                 MainTuristicPoint.setUpScreen(touristicPoint);
                 Intent intentGoPointScreen = new Intent(MainActivity.this, MainTuristicPoint.class);
                 finish();
@@ -89,6 +91,33 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        search = (SearchView) MenuItemCompat.getActionView(searchItem);
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.length() > 0) {
+                    ArrayList<TouristicPoint> listAux = new ArrayList<>();
+                    for (TouristicPoint point : touristicPoints) {
+                        if (point.getName().toLowerCase().contains(newText.toLowerCase())) {
+                            listAux.add(point);
+                        }
+                    }
+                    touristicPoints = listAux;
+                    populate();
+                }
+                else {
+                    touristicPoints = allPoints;
+                    populate();
+                }
+                return false;
+            }
+        });
         return super.onCreateOptionsMenu(menu);
     }
 
